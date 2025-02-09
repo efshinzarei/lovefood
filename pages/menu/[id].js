68f866c6324b1d309d9547162a3314e9d6b81ece
Afshin Zarei <afshin.zarei2000@gmail.com>
2025-02-09 18:46:27 +0330

    Added .env url

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -8,14 +8,14 @@ function Detailes({ data }) {
   if (router.isFallback) {
     return <h2>Loading Page...</h2>;
   }
-  
+
   return <DetailsPage {...data} />;
 }
 
 export default Detailes;
 
 export async function getStaticPaths() {
-  const res = await fetch("http://localhost:4000/data");
+  const res = await fetch(`${process.env.BASE_URL}/data`);
   const json = await res.json();
   const data = json.slice(0, 10);
 
@@ -32,17 +32,17 @@ export async function getStaticProps(context) {
     params: { id },
   } = context;
 
-  const res = await fetch(`http://localhost:4000/data/${id}`);
+  const res = await fetch(`${process.env.BASE_URL}/data/${id}`);
   const data = await res.json();
 
   if (!data.id) {
     return {
-      notFound: false,
+      notFound: true,
     };
   }
 
   return {
     props: { data },
-    revalidate: +process.env.REVALIDATE, // seconds
+    revalidate: process.env.REVALIDATE, // seconds
   };
 }
