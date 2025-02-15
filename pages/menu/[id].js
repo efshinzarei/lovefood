2441c3f834d5af45de1f9b258f49db686660ff73
Afshin Zarei <afshin.zarei2000@gmail.com>
2025-02-15 15:23:28 +0330

    Added public url

--- pages/menu/[id].js
+++ pages/menu/[id].js
@@ -15,7 +15,7 @@ function Detailes({ data }) {
 export default Detailes;
 
 export async function getStaticPaths() {
-  const res = await fetch(`${process.env.BASE_URL}/data`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data`);
   const json = await res.json();
   const data = json.slice(0, 10);
 
@@ -32,7 +32,7 @@ export async function getStaticProps(context) {
     params: { id },
   } = context;
 
-  const res = await fetch(`${process.env.BASE_URL}/data/${id}`);
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/data/${id}`);
   const data = await res.json();
 
   if (!data.id) {
